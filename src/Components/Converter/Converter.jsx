a970c74bd70ed29c7ecab06fa0b6a9d0034bec4e
Matias Cejas <matiascejas8@hotmail.com>
2019-08-18 01:25:59 -0300

    UI Setup ready

--- src/Components/Converter/Converter.jsx
+++ src/Components/Converter/Converter.jsx
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
+import { Card, InputGroup, FormControl, Table } from "react-bootstrap";
+import Dollar from "../../assets/dollar.png";
+import Euro from "../../assets/euro.png";
+import BitCoin from "../../assets/bitcoin.png";
 import axios from "axios";
 
 export default class Converter extends Component {
   state = {
     arsToUsd: 0,
     arsToEur: 0,
+    usdConvert: 0,
   };
   componentDidMount() {
     axios
@@ -13,18 +18,92 @@ export default class Converter extends Component {
       )
       .then(res => {
         this.setState({
-          arsToUsd: res.data.USD_ARS,
-          arsToEur: res.data.EUR_ARS,
+          arsToUsd: Math.round(res.data.USD_ARS * 100) / 100,
+          arsToEur: Math.round(res.data.EUR_ARS * 100) / 100,
         });
       });
   }
-
-  //   request_2() {
-  //     return axios.get(
-  //       "https://free.currconv.com/api/v7/convert?apiKey=fcba8c3eabd5419de1c7&q=USD_ARS,EUR_ARS&compact=ultra&date=2019-08-05&endDate=2019-08-13",
-  //     );
-
+  handleChange = e => {
+    this.setState({ [e.target.usdConvert]: e.target.value });
+  };
   render() {
-    return <div>{this.state.arsToEur}</div>;
+    return (
+      <div>
+        <br />
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <Card style={{ width: "10rem" }}>
+                <Card.Img variant="top" src={Dollar} />
+                <Card.Body>
+                  <Card.Title>Dolar</Card.Title>
+                  <Card.Text>${this.state.arsToUsd}</Card.Text>
+                </Card.Body>
+              </Card>
+            </div>
+            <div className="col">
+              <Card style={{ width: "10rem" }}>
+                <Card.Img variant="top" src={Euro} />
+                <Card.Body>
+                  <Card.Title>EURO</Card.Title>
+                  <Card.Text>${this.state.arsToEur}</Card.Text>
+                </Card.Body>
+              </Card>
+            </div>
+
+            <div className="col">
+              <Card style={{ width: "10rem" }}>
+                <Card.Img variant="top" src={BitCoin} />
+                <Card.Body>
+                  <Card.Title>BitCoin</Card.Title>
+                  <Card.Text>
+                    ${Math.round(this.state.arsToUsd * 10144.965)}
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </div>
+          </div>
+          <hr />
+          <div className="row">
+            <InputGroup className="mb-3">
+              <InputGroup.Prepend>
+                <InputGroup.Text
+                  type="number"
+                  value={this.state.usdConvert}
+                  onChange={this.handleChange}
+                >
+                  $
+                </InputGroup.Text>
+              </InputGroup.Prepend>
+              <FormControl aria-label="Amount (to the nearest dollar)" />
+            </InputGroup>
+          </div>
+          <div className="row">
+            <Table responsive striped bordered hover>
+              <thead>
+                <tr>
+                  <th colSpan="2">Moneda</th>
+                  <th>Valor</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td colSpan="2">Dolar</td>
+                  <td>{this.usdConvert}</td>
+                </tr>
+                <tr>
+                  <td colSpan="2">Euro</td>
+                  <td>200</td>
+                </tr>
+                <tr>
+                  <td colSpan="2">Bitcoin</td>
+                  <td>200</td>
+                </tr>
+              </tbody>
+            </Table>
+          </div>
+        </div>
+      </div>
+    );
   }
 }

4d57a4e7180f9bdfd39d4106b754d345780c229e
Matias Cejas <matiascejas8@hotmail.com>
2019-08-18 14:18:31 -0300

    Updates

---
+++ precache-manifest.f92d8bb83ec4b88708ace68ab6a3fc2b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95d43f9b3f4c03da299cf812b43d26cb",
+    "url": "/currency-converter-react/index.html"
+  },
+  {
+    "revision": "c3f06716f52a9ebc676b",
+    "url": "/currency-converter-react/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "3061dd8a229e04ba7538",
+    "url": "/currency-converter-react/static/css/main.899940fc.chunk.css"
+  },
+  {
+    "revision": "c3f06716f52a9ebc676b",
+    "url": "/currency-converter-react/static/js/2.34520773.chunk.js"
+  },
+  {
+    "revision": "3061dd8a229e04ba7538",
+    "url": "/currency-converter-react/static/js/main.b5b3e42a.chunk.js"
+  },
+  {
+    "revision": "e4683a23c0be0d4c2acd",
+    "url": "/currency-converter-react/static/js/runtime~main.2eb00760.js"
+  },
+  {
+    "revision": "a63ede78812f7a03192b674e552c1c1c",
+    "url": "/currency-converter-react/static/media/dollar.a63ede78.png"
+  }
+]);
